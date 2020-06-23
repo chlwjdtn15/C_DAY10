beeccde66e164cb495d0483da0aec8a17ae30844
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-06-23 02:42:58 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+// 배열에 10개의 양의 정수를 입력 받아서 저장하고, 저장된 정수 중 짝수 정수의 합을 출력하시오.
+#include <stdio.h>
+
+int main(void) {
+
+  // v1 아래 배열의 값중 짝수의 합만 출력.
+  int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+  int sum = 0;
+  for (int i = 0; i <= arr[i]; i++) {
+    
+    if (arr[i] % 2 == 0) {
+      
+
+      sum += arr[i];
+    }
+
+    printf("%d", sum);
+
+  }
+  return 0;
+}
