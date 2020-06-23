7dd68e831bdb6e271c7e813c214c056a9f492d57
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-06-23 03:07:09 +0000

    scanf 기초

--- main.c
+++ main.c
@@ -15,23 +15,36 @@ int main(void) {
 
 
   // v1 아래 배열의 값중 짝수의 합만 출력.
-  int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-  int sum = 0;
-  int size = sizeof(arr) / sizeof(arr[0]);
+  // int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+  // int sum = 0;
+  // int size = sizeof(arr) / sizeof(arr[0]);
 
-  printf("%d\n", size);
-  for (int i = 0; i < size; i++) {
+  // printf("%d\n", size);
+  // for (int i = 0; i < size; i++) {
     
-    if (arr[i] % 2 == 0) {
+  //   if (arr[i] % 2 == 0) {
       
 
-      sum += arr[i];
-    }
+  //     sum += arr[i];
+  //   }
+  // }
 
-    
+  // printf("%d", sum);
 
-  }
 
-  printf("%d", sum);
+
+
+  int b = 0;
+  
+  printf("my_scan() 사용 전 : %d\n", b);
+  
+
+  scanf("%d", &b);
+
+  
+  printf("my_scan() 사용 후 : %d\n", b); // 출력 : 123
+
+
+
   return 0;
 }
